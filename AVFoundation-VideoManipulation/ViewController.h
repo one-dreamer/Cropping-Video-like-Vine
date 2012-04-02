7350a60ada0720373a1f1e2bef75e8727625424f
Abdul Azeem Khan <azeem@Azeem-Mac.local>
2012-04-02 11:13:16 +0500

    Initial Commit

---
+++ AVFoundation-VideoManipulation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AVFoundation-VideoManipulation
+//
+//  Created by Abdul Azeem Khan on 4/2/12.
+//  Copyright (c) 2012 DataInvent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
